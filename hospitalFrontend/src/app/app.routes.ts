b07a5d12a2dd47a20d1d77e5c3d4c00de40b51e4
Samantha <samiiponce03@gmail.com>
2025-11-28 16:24:22 +0100

    create #1 Home page

--- hospitalFrontend/src/app/app.routes.ts
+++ hospitalFrontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { LoginComponent } from './pages/login/login';
+import { NursesComponent } from './pages/nurses/nurses';
+import { FindNurseComponent } from './pages/find-nurse/find-nurse';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'nurses', component: NursesComponent },
+  { path: 'find-nurse', component: FindNurseComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
