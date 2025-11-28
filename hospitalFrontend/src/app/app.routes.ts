70c73b234b20ab902c088e6214b446f0427b629e
Adrivzq <adristucom@gmail.com>
2025-11-28 17:00:41 +0100

    fix#4 create find nurse find component

--- hospitalFrontend/src/app/app.routes.ts
+++ hospitalFrontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FindNurse } from './find-nurse/find-nurse';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'find-nurse', pathMatch: 'full' },
+  { path: 'find-nurse', component: FindNurse }
+];
