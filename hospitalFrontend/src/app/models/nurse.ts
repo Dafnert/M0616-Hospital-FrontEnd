417d8706ceec018db9bc82deba838df6407656fe
Dafnert <dafne.rt04@gmail.com>
2025-12-16 16:03:28 +0100

    Fix #22 Add NurseService for the login

---
+++ hospitalFrontend/src/app/models/nurse.ts
@@ -0,0 +1,18 @@
+export class Nurse{
+    id:number=0;
+    name:string="";
+    surname:string="";
+    age:number=0;
+    speciality:string="";
+    username:string="";
+    password:string="";
+    constructor(id =0,name="",surname="", age =0, speciality="", username="", password="" ){
+        this.id=id;
+        this.name=name;
+        this.surname=surname;
+        this.age=age;
+        this.speciality=speciality;
+        this.username=username;
+        this.password=password;
+    }
+}
