749ff43bd3434763047f15a2a4ff8ba6b17bb801
ferranbustos <ferribb06@gmail.com>
2025-12-21 22:33:42 +0100

    #17 listnurses with image

--- hospitalFrontend/src/app/pages/listnurses/listnurses.ts
+++ hospitalFrontend/src/app/pages/listnurses/listnurses.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
+interface Nurse {
+  id: number;
+  name: string;
+  specialty: string;
+  img: string;
+}
 
 @Component({
   selector: 'app-listnurses',
+  standalone: true,
+ 
   templateUrl: './listnurses.html',
-  styleUrls: ['./listnurses.css'],
+  styleUrls: ['./listnurses.css']
 })
-export class ListnursesComponent {
+export class ListnursesComponent implements OnInit {
+  title: string = "Hospital Management";
   logoAlt: string = "Logo hospital";
   
-  nurses = [
-    { id: 1, name: 'Ferran Bustos', specialty: 'Pediatría' },
-    { id: 2, name: 'Dafne Vicente', specialty: 'Urgencias' },
-    { id: 2, name: 'Adria Vasquez', specialty: 'Urgencias' },
-    { id: 3, name: 'Samantha', specialty: 'Quirófano' }
+  nurses: Nurse[] = [
+    { id: 1, name: 'Ferran Bustos', specialty: 'Cardiología', img: 'img/images.jpg' },
+    { id: 2, name: 'Dafne Vicente', specialty: 'Urgencias', img: 'img/images.jpg' },
+    { id: 3, name: 'Adria Vasquez', specialty: 'Urgencias', img: 'img/images.jpg' },
+    { id: 4, name: 'Samantha', specialty: 'Quirófano', img: 'img/images.jpg' }
   ];
+
+  ngOnInit(): void {
+    
+    console.log('Número de enfermeros:', this.nurses.length);
+  }
+
+  toggleView(): void {
+    this.title = "Lista actualizada de enfermeros";
+    console.log('Vista actualizada');
+  }
 }
